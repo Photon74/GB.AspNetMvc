b1349e48c52d1dde72c111a01e69a196f7b579b5
Александр Колодинский <photon.74@gmail.com>
2022-06-21 23:43:37 +0300

    Some fix

--- GB.AspNetMvc/Models/DTO/ProductDto.cs
+++ GB.AspNetMvc/Models/DTO/ProductDto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
 using System.Collections;
 using System.ComponentModel.DataAnnotations;
 
@@ -11,7 +12,7 @@ namespace GB.AspNetMvc.Models.DTO
 
         [Required]
         [Display(Name ="Наименование товара")]
-        [StringLength(255, MinimumLength = 3)]
+        [StringLength(255, MinimumLength = 3, ErrorMessage ="Название должно быть не короче 3-х символов!")]
         public string Name { get; set; }
 
         [Required]
